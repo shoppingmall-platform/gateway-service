24f55f4f0ef2c47f81e64647e984e2df199a3fde
qaw302 <osshun21@gmail.com>
2026-01-19 16:03:46 +0900

    에러메시지 스텍추가

--- src/main/java/com/smplatform/gateway_service/handler/ExceptionHandler.java
+++ src/main/java/com/smplatform/gateway_service/handler/ExceptionHandler.java
@@ -35,7 +35,7 @@ public class ExceptionHandler implements ErrorWebExceptionHandler {
         } else {
             response.setStatusCode(HttpStatus.NOT_FOUND);
         }
-        log.error("에러 메시지 : {}",ex.getMessage());
+        log.error("에러 메시지 : {}",ex.getMessage(), ex);
 
         return response.writeWith(
                 Mono.just(response
